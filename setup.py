1008e098366908965f3d3f5e80c987b4a8f59939
RicardozzZ <GeekRicardozzZ@gmail.com>
2024-01-10 18:02:11 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
+import configparser
 from setuptools import setup, find_packages
 
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
+def read_version():
+    config = configparser.ConfigParser()
+    config.read('.bumpversion.cfg')
+    return config['bumpversion']['current_version']
+
 setup(
     name="dserver",
-    version="0.1.9",
+    version=read_version(),
     packages=find_packages(),
     author="Geek Ricardo",
     author_email="GeekRicardozzZ@gmail.com",
